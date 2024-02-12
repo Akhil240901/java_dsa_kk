4b7831b60bec73f763af6bb08fef4e3919ec4a60
Akhil Bisen <bisenakhil2001@gmail.com>
2024-02-12 16:45:29 +0530

    fibonacci program

---
+++ Code/src/fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a=0;
+        int b=1;
+        System.out.println("Enter the no of element you want:");
+        int n=input.nextInt();
+        while(n>0){
+            System.out.println(b);
+            int temp=b;
+            b=a+b;
+            a=temp;
+            n--;
+        }
+    }
+}
