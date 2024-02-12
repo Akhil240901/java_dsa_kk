b6313acb0c98cf42a14045c48b219b3ed61586fa
Akhil Bisen <bisenakhil2001@gmail.com>
2024-02-12 16:40:06 +0530

    fibonacci program

---
+++ Code/src/lowercase_uppercase.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class lowercase_uppercase {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the text");
+        char ch=input.next().trim().charAt(0);
+        if(ch >= 'a' && ch <= 'z'){
+            System.out.println("It is in lowercase ");
+        }
+        else{
+            System.out.println("It is in uppercase");
+        }
+
+    }
+}
