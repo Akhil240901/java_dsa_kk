3b76dd7d8c064182dc6bedba25bd5a395cb7291f
Akhil Bisen <bisenakhil2001@gmail.com>
2024-03-17 21:56:22 +0530

    binary and order agnostic search problem

---
+++ Code/src/BinarySearch/ceilingNumber.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class ceilingNumber {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,7,11,15,34,36,38};
+        System.out.println(findCeil(arr,14)) ;
+    }
+
+     static int findCeil(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        int mid;
+
+        if(arr.length==0){
+            return -1;
+        }
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(target<arr[mid]){
+                end=mid-1;   //if target element is lies in left side
+            }else if(target>arr[mid]){
+                start=mid+1; //if target element is lies right side
+            }else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
