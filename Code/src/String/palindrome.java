7e440384f8cdc63427c36c02617623b10e83461d
Akhil Bisen <bisenakhil2001@gmail.com>
2024-04-03 09:18:46 +0530

    palindrome

---
+++ Code/src/String/palindrome.java
@@ -0,0 +1,20 @@
+package String;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String str="c";
+        System.out.println(palindromeCheck(str));
+    }
+
+     static boolean palindromeCheck(String str) {
+        if (str.length()==0){
+            return true;
+        }
+       for(int i=0;i<str.length()/2;i++){
+           if (str.charAt(i)!=str.charAt(str.length()-1-i)){
+               return false;
+           }
+       }
+       return  true;
+    }
+}
