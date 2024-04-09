a7ea6504de138e5dd706e2b3da751068d18e5257
Akhil Bisen <bisenakhil2001@gmail.com>
2024-04-10 00:19:02 +0530

    reversing string

--- Code/src/String/reverseString.java
+++ Code/src/String/reverseString.java
@@ -1,4 +1,19 @@
 package String;
 
+import java.util.Locale;
+
 public class reverseString {
+    public static void main(String[] args) {
+        String str="Akhil";
+        reverse(str);
+    }
+
+    static void reverse(String str) {
+        str=str.toLowerCase();
+        String rev_str="";
+        for (int i=str.length()-1;i>=0;i--){
+            rev_str=rev_str+str.charAt(i);
+        }
+        System.out.println(rev_str);
+    }
 }
