1e6f61ce1e316ec9a0cd94ec69bc692186117665
Akhil Bisen <bisenakhil2001@gmail.com>
2024-06-09 15:59:47 +0530

    Queue implementation using LL

---
+++ Code/src/Queue/implementation.java
@@ -0,0 +1,16 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class implementation {
+    public static void main(String[] args) {
+        Queue<Integer> q=new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.peek());
+        System.out.println(q);
+
+    }
+}
