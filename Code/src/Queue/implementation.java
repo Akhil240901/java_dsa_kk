ca130bfbdda3218e5fa20e558202461fd5737336
Akhil Bisen <bisenakhil2001@gmail.com>
2024-06-17 09:14:02 +0530

    implementation

--- Code/src/Queue/implementation.java
+++ Code/src/Queue/implementation.java
@@ -6,9 +6,15 @@ import java.util.Queue;
 public class implementation {
     public static void main(String[] args) {
         Queue<Integer> q=new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
+        //offers element to add and use instead of add due exception throw while queue is empty
+        q.offer(20);
+        q.offer(30);
+        q.offer(40);
+        System.out.println();
+
+        //.poll() remove the head element(first element)
+        q.poll();
+        //peek() will return first element
         System.out.println(q.peek());
         System.out.println(q);
 
