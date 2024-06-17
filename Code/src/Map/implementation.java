0f8485400e46f759724c54f4b6e243bf3dcc6ff4
Akhil Bisen <bisenakhil2001@gmail.com>
2024-06-17 16:23:05 +0530

    implementation of all queue and set

---
+++ Code/src/Map/implementation.java
@@ -0,0 +1,30 @@
+package Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class implementation {
+    public static void main(String[] args) {
+        //add key and value ,but in random order
+          //Map<String,Integer> mp=new HashMap<>();
+
+        //add key value in orederd sorted way
+        Map<String,Integer> mp=new TreeMap<>();
+        //put method adds key,value pair
+        mp.put("india",140);
+        mp.put("china",140);
+        mp.put("australia",30);
+        mp.put("myanmar",10);
+
+        //.containsKey and containsValue returns true or false
+        System.out.println(mp.containsKey("india"));
+
+        if(!mp.containsKey("pakistan")){
+            mp.put("pakistan",80);
+        }
+        //either use putIfAbsent instead if condition
+        mp.putIfAbsent("bhutan",1);
+        System.out.println(mp);
+    }
+}
